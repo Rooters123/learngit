1330af8a2bc63e215da1d238acea04145c88995c
zbr <18332672733@163.com>
2021-01-09 21:05:50 +0800

    右上角显示用户登录

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,10 +1,24 @@
+from info import redis_store
+from info.models import User
 from . import index_blue
-from flask import render_template,current_app
+from flask import render_template,current_app,session
 @index_blue.route("/")
 def index():
     # redis_store.set("name","zbr")
     # print(redis_store)
-    return render_template("news/index.html")
+    try:
+        user_id = session.get("user_id")
+    except Exception as e:
+        current_app.logger.error(e)
+    if user_id:
+        user = User.query.get(user_id)
+        user_info = user.to_dict()
+    else:
+        user_info = None
+    data = {
+        "user_info":user_info
+    }
+    return render_template("news/index.html",data = data)
 
 
 @index_blue.route('/favicon.ico')
